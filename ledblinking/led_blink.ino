07edfc4255df5924be4cc48c4996bf5fdfe5344d
MADHUSUDHAN AMUDALA <amadhusudhan2016@gmail.com>
2022-07-12 14:13:23 +0530

    Update led_blink.ino

--- ledblinking/led_blink.ino
+++ ledblinking/led_blink.ino
@@ -1,17 +1,16 @@
-const int led =22;
-
+const int led =33;
 void setup() {
   // put your setup code here, to run once:
  pinMode(led,OUTPUT);
- Serial.begin(9600);
+ Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 digitalWrite(led,HIGH);
 Serial.print("LED IS ON\n");
-delay(5000);
+delay(500);
 digitalWrite(led,LOW);
 Serial.print("LED IS OFF\n");
-delay(5000);
+delay(500);
 }
